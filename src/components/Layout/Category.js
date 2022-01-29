ea861e8876f7e84765544e1afdd0385fba065b93
sachinmewar <sachinmewar800@gmail.com>
2022-01-29 10:42:55 +0530

    worked in Category js and css file

---
+++ src/components/Layout/Category.js
@@ -0,0 +1,13 @@
+
+import './Category.css';
+const Category = () => {
+    return(
+        <select className = 'category-container'>
+            <option> Electronic's</option>
+            <option> Men's Clothing </option>
+            <option> Women's Clothing </option>
+        </select>
+    );
+}
+
+export default Category;
