65152f69a0843dc1fdf1e20f52a75794d79e9ed4
sachinmewar <sachinmewar800@gmail.com>
2022-01-31 22:04:05 +0530

    return list of products

---
+++ src/components/Product/Product.js
@@ -0,0 +1,11 @@
+import ProductList from "./ProductList";
+
+const Product = () => {
+    return(
+        <div className="product-container">
+            <ProductList />
+        </div>
+    )
+}
+
+export default Product;
