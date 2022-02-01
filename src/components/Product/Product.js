03ce18a5e129f85c2a7a0c160acf6f4d8523d525
sachinmewar <sachinmewar800@gmail.com>
2022-02-01 22:16:07 +0530

    added url state and send url to ProductList

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,22 @@
 import ProductList from "./ProductList";
+import useFetch from "../../hooks/use-fetch";
+import { useState } from "react";
 
-const Product = () => {
+const Product = ({selectedCategory}) => {
+
+    const [url, setUrl] = useState('https://fakestoreapi.com/products');
+
+    console.log(selectedCategory);
+
+    // if (selectedCategory != 'all') {
+    //     const tempUrl = 'https://fakestoreapi.com/products/category' + selectedCategory;
+    //     setUrl(tempUrl);
+    // }
+
+
     return(
         <div className="product-container">
-            <ProductList />
+           <ProductList url = {url}/>
         </div>
     )
 }
