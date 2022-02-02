777db7be4799ac6ea7338c4739a13fbe8b71f3c3
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:32:18 +0530

    Added SearchButton for SearchBar

---
+++ src/components/Layout/SearchButton.js
@@ -0,0 +1,11 @@
+import SearchIcon from "./SearchIcon";
+
+const SearchButton = () => {
+    return(
+        <button className="search-button" type="submit">
+            <SearchIcon />
+        </button>
+    )
+}
+
+export default SearchButton;
