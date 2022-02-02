a45ccbc2cfa35a06aa9bb19a04786b445b722dd9
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:33:16 +0530

    Added WishlistButton

---
+++ src/components/Layout/WishlistButton.js
@@ -0,0 +1,11 @@
+import './WishlistButton.css';
+
+const WishlistButton = () => {
+    return(
+        <button className='wishlist-button'>
+            <span id= 'text'> Wishlist </span>
+        </button>
+    )
+}
+
+export default WishlistButton;
