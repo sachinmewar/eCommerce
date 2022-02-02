4f1b115d301235d8d5aafa5411bdecfacb4ed9d4
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:37:45 +0530

    Added selectedCategory logic

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,40 @@
 import ProductList from "./ProductList";
 import useFetch from "../../hooks/use-fetch";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import ProductDetail from "../ProductDetail/ProductDetail";
 
 const Product = ({selectedCategory}) => {
 
     const [url, setUrl] = useState('https://fakestoreapi.com/products');
+    const [isImageClicked, setIsImageClicked] = useState(false);
 
     console.log(selectedCategory);
 
-    // if (selectedCategory != 'all') {
-    //     const tempUrl = 'https://fakestoreapi.com/products/category' + selectedCategory;
-    //     setUrl(tempUrl);
-    // }
+    let tempUrl;
 
+     if (selectedCategory === 'all') {
+         tempUrl = 'https://fakestoreapi.com/products';
+     }
+     else{
+         tempUrl = 'https://fakestoreapi.com/products/category/' + selectedCategory;
+     }
+
+    const onImageClickHandler = (productDetails) => {
+        setIsImageClicked(productDetails[0])
+        setUrl('https://fakestoreapi.com/products/' + productDetails[1])
+    }
 
     return(
         <div className="product-container">
-           <ProductList url = {url}/>
+            { !isImageClicked && 
+            <ProductList url = {tempUrl} onImageClick = {onImageClickHandler}/>
+            }
+
+            {
+               isImageClicked &&
+                  <ProductDetail url = {url}/>
+            }
         </div>
     )
 }
