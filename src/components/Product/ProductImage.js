0972df82401ba148e9777847abd37faaca1f41d0
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:38:35 +0530

    Added imageClickHandler funtion

--- src/components/Product/ProductImage.js
+++ src/components/Product/ProductImage.js
@@ -2,9 +2,14 @@ import './ProductImage.css';
 
 const ProductImage = (props) => {
     const imageUrl = props.imageUrl;
+
+    const onImageClickHandler = () => {
+        props.onImageClick(true)
+    } 
+
     return(
         <div className="image-container">
-            <img src={imageUrl} alt="Failed to load image.." />
+            <img src={imageUrl} alt="Failed to load image.."  onClick={onImageClickHandler}/>
         </div>
     )
 }
