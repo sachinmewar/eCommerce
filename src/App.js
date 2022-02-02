3dcca44a0f0314c826f6834e6d4d4c7310242782
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:41:34 +0530

    commited changes in App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import Navigation from './components/Layout/Navigation';
+import Product from './components/Product/Product';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 
 function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const setSelectedCategoryValueHandler = (categoryFilterValue) => {
+    setSelectedCategory(categoryFilterValue);
+  }
+  
   return (
-    <Navigation> </Navigation>
-  );
+    <div className='app'>
+      <Navigation setCategoryFilterValue = {setSelectedCategoryValueHandler} />
+      <br />
+      <Switch>
+        <Route path = "/">
+          <Redirect to="/product" />
+          <Product selectedCategory = {selectedCategory}/>
+        </Route>
+
+        {/* { <Route path = "/product">
+          <Product selectedCategory = {selectedCategory}/>
+        </Route> } */}
+        
+        <Route path="/productDetails">
+          
+        </Route>
+
+        <Route path = "*">
+          <Redirect to= "/" />
+        </Route>
+      </Switch>
+    </div>
+    );
 }
 
 export default App;
