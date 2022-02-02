54da81e67f15b3dc5963c981124626a767bb8ec0
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:39:55 +0530

    Passes onImageClick with props

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -8,21 +8,25 @@ const ProductList = (props) => {
     const {data, loading} = useFetch(url);
     var productData;
 
+    const onImageClickHandler = (productDetails) => {
+        props.onImageClick(productDetails)
+    }
+
     if(data != null){
         productData = data.map( (productValue) =>
             <ProductItem 
                key = {productValue.id}
+               id = {productValue.id}
                name = {productValue.title}
                category = {productValue.category}
                description = {productValue.description}
                imageUrl = {productValue.image}
                rating = {productValue.rating}
                price = {productValue.price}
+               onImageClick = {onImageClickHandler}
             />
         )
     }
-    
-    console.log();
 
     return(
         <div className="productList-container">
