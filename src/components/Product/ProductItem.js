c5d62b93ceb1a8347b98e5b4faa8a31ab96c47eb
sachinmewar <sachinmewar800@gmail.com>
2022-02-02 21:39:01 +0530

    Added onImageClickHandler funtion

--- src/components/Product/ProductItem.js
+++ src/components/Product/ProductItem.js
@@ -3,9 +3,13 @@ import './ProductItem.css';
 
 const ProductItem = (props) => {
     var price = '$' + props.price;
+
+    const onImageClickHandler = (value) => {
+        props.onImageClick([value, props.id]);
+    }
     return(
         <div className="productItem-container">
-            <ProductImage imageUrl = {props.imageUrl}/>
+            <ProductImage imageUrl = {props.imageUrl} onImageClick = {onImageClickHandler}/>
             <div id = "name"> {props.name} </div>
             <div id="price"> {price} </div>
             <button> Wishlist </button>
